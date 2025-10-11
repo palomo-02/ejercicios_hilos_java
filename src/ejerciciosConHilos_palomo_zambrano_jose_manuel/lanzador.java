750312f7fadb68f6d3ea04760da2cddbdc70b375
usuario <palomo.zambrano.jose.manuel@iescamas.es>
2025-10-11 05:46:12 +0200

    estado runnable

--- src/ejerciciosConHilos_palomo_zambrano_jose_manuel/lanzador.java
+++ src/ejerciciosConHilos_palomo_zambrano_jose_manuel/lanzador.java
@@ -2,13 +2,31 @@ package ejerciciosConHilos_palomo_zambrano_jose_manuel;
 
 public class lanzador {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 
 
-		Thread h = new Thread(new PruebasConHilos("nuevo"));
+		Thread h = new Thread(new PruebasConHilos("a"));
+
+		h.start();
 		
 		
-		System.out.println(h.getName() + "------->"+ h.getState());
+		try {
+
+
+		
+	System.out.println(h.getName() + "------->"+ h.getState());
+
+	Thread.sleep(50);
+	System.out.println(h.getName() + "------->"+ h.getState());
+		
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
 		
 	}
 
