e56fe65356bac0b676f76cf1e79f79dbb378a33e
usuario <palomo.zambrano.jose.manuel@iescamas.es>
2025-11-13 22:56:01 +0100

    añadido ejercicio7

---
+++ src/com/Ejercicio7.java
@@ -0,0 +1,40 @@
+package com;
+
+public class Ejercicio7 {
+	public static void main(String[] args) {
+
+		Thread t1 = new Thread(() -> {
+
+			int contador = 0;
+
+			while (!Thread.currentThread().isInterrupted()) {
+
+				contador++;
+				System.out.println("contador" + contador);
+				try {
+					Thread.sleep(500);
+
+				} catch (InterruptedException e) {
+
+					System.out.println("el hilo ha sido interrrumpido ");
+					
+					
+					
+					
+				}
+
+			}
+			System.out.println("el hilo ha finalizado");
+		});
+		t1.start();
+		try {
+			Thread.sleep(2000);
+
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+		}
+		t1.interrupt();
+		
+	}
+
+}
